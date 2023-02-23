b58f88b7a0ca2d5046d4afc9a7fb9a35901ab2fc
Rezurgat <Killa.S.V.ru@bk.ru>
2023-02-23 19:16:45 +0300

    Added author model, author serializer, media folder(refactored settings)

--- news_api/news/models.py
+++ news_api/news/models.py
@@ -13,6 +13,20 @@ class Category(models.Model):
         ordering = ['title']
 
 
+class Author(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    avatar = models.ImageField(upload_to='avatars/', null=True)
+    position = models.CharField(max_length=50)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
+
+    class Meta:
+        ordering = ['firstname']
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
@@ -27,11 +41,12 @@ class Tag(models.Model):
 
 class News(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
+    image = models.ImageField(upload_to='news_media/', null=True)
     content = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, related_name='news_category', on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, related_name='news_author', on_delete=models.SET_DEFAULT, null=True, default='Null')
     tag = models.ForeignKey(Tag, related_name='news_tag', on_delete=models.SET_DEFAULT, null=True, default='Null')
 
     def __str__(self):
--- news_api/news/serializers.py
+++ news_api/news/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
-from .models import Category, Tag, News
+from .models import Category, Tag, Author,  News
 
 
-class СategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'title', 'create_at',)
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
 
 
 class TagSerializers(serializers.ModelSerializer):
