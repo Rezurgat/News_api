80c7cbd3d8f5008a2c025d6e73b9cdd8250b0638
Rezurgat <Killa.S.V.ru@bk.ru>
2023-02-23 20:09:20 +0300

    Created views classes, registrated router and urls, refactored admin file

--- news_api/config/urls.py
+++ news_api/config/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework.routers import DefaultRouter
+from news import views
+
+router = DefaultRouter()
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'authors', views.AuthorViewSet)
+router.register(r'tags', views.TagViewSet)
+router.register(r'news', views.NewsViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    re_path(r'^', include(router.urls)),
+    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
--- news_api/news/views.py
+++ news_api/news/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Category, Tag, Author, News
+from .serializers import CategorySerializer, AuthorSerializer, TagSerializer, NewsSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class NewsViewSet(viewsets.ModelViewSet):
+
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
